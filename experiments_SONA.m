aa43f27bccbaf3e47a047ebb4262bc4c3d5dfa24
Ferrer Mestres, Jonathan (L&W, Dutton Park) <Jonathan.Ferrermestres@csiro.au>
2020-10-26 18:08:16 +1000

    Minor changes on experiments SONA file

--- experiments_SONA.m
+++ experiments_SONA.m
@@ -4,61 +4,22 @@ p = 0.00000000000001;
 %Discount factor
 discount = 0.96;
 
-
+%Load transition and reward matrices
 load('problems/SONA/SONA_P_500_sim.mat');
 load('problems/SONA/SONA_R_500_sim.mat');
-
-
-S = readmatrix('problems/SONA/results/states.txt');
-S = S(:,1:4);
-
-
-%Explote data
-    
-min_dens_aba = S(:,1);
-max_dens_aba = S(:,2);
-min_abun_so = S(:,3);
-max_abun_so = S(:,4);
-states = [1:819];
-
-
-
-[min_dens_aba_u] = unique(min_dens_aba);
-[max_dens_aba_u] = unique(max_dens_aba);
-[min_abun_so_u] = unique(min_abun_so);
-[max_abun_so_u] = unique(max_abun_so);
-
 
 
 P = Tr;
 NS = size(P,1);
 
 %Abstract state space size
-%K = [round(NS), round(NS/1.25), round(NS/1.5), round(NS/2), round(NS/3), round(NS/4), round(NS/6), round(NS/8), round(NS/10), round(10), round(5)];
-
-K = [25:50:NS];
-K = flip(K);
-K = [NS, K, 5];
-%K = [5];
-
-
-
-actions = strings(size(P,3), 1);
-actions(1) = "I";
-actions(2) = "AP";
-actions(3) = "C";
-actions(4) = "H";
-min_prob = 0.001;
+K = [round(NS), round(NS/1.25), round(NS/1.5), round(NS/2), round(NS/4)];
 
 
 tic;
+
 %Perform value iteration
 [Pol]=mdp_value_iteration(P, R, discount);%Get a policy using value iteration
-
-save('Pol.mat', 'Pol');
-
-
-
 %Evaluate the policy
 [V,Q]= mdp_eval_policy_iterative_q(P, R, discount, Pol); % check that this is doing what it is supposed to; Get the value and Q values
 
@@ -68,12 +29,8 @@ Q(isnan(Q)) = 0;
 
 
 
-
-
-%fprintf('KMEANS\n');
-%[PK_kmeans, RK_Kmeans, S2K_kmeans, K2S_kmeans, PolicyK_kmeans, PolKs_kmeans, err_kmeans, t_kmeans, t_KMDP_kmeans] = kmeansKMDP(200, P, R, discount, Q, V);
-
 %Initialize structures for statistics extraction
+
 err_transitive = zeros(length(K),1);
 t_transitive = zeros(length(K),1);
 t_KMDP_transitive = zeros(length(K),1);
@@ -91,7 +48,6 @@ t_kmeans = zeros(length(K),1);
 t_KMDP_kmeans = zeros(length(K),1);
 
 
-
 S2K_Qd = cell(length(K),1);
 K2S_Qd = cell(length(K),1);
 
@@ -99,31 +55,32 @@ S2K_astar = cell(length(K),1);
 K2S_astar = cell(length(K),1);
 
 
+%RUN the algorithms
+
+
 for i = 1:length(K)
     
-    k = K(i);
-    
-    
+  k = K(i);
+   
+ fprintf('K-means \n');
  [PK_kmeans, RK_Kmeans, S2K_kmeans, K2S_kmeans, PolicyK_kmeans, PolKs_kmeans, err_kmeans(i), t_kmeans(i), t_KMDP_kmeans(i)] = kmeansKMDP(k, P, R, discount, Q, V);
     
- %fprintf('TRANSITIVE d\n');
+ fprintf('Q d\n');
  [PK_Qd, RK_Qd, S2K_Qd{i}, K2S_Qd{i}, PolicyK_Qd, PolKs_Qd, err_transitive(i), t_transitive(i), t_KMDP_transitive(i)] = QdKMDP(k, p, P, R, discount, Q, V);
                 
- %fprintf('TRANSITIVE ASTAR\n');
+ fprintf('TRANSITIVE ASTAR\n');
  [PK_astar, RK_astar, S2K_astar, K2S_astar, PolicyK_astar, PolKs_astar, err_astar(i), t_astar(i), t_KMDP_astar(i)] = aStarKMDP(k, p, P, R, discount, V, Pol); 
 
- P(isnan(P)) = 0;
- %P(P==0) = 0.0001;
-
-    
     
 end
 
 
+%****************************************************************************************************************************
+
+%PLOT RESULTS: error, times and state space representations
+
+
 %DATA to plot and extract
-
-
-K
 
 disp('Q_d')
 err_transitive
@@ -143,6 +100,9 @@ t_KMDP_kmeans
 
 %Store relevant data
 
+
+save('problems/SONA/results/S2K_kmeans.mat', 'S2K_kmeans');
+save('problems/SONA/results/K2S_kmeans.mat', 'K2S_kmeans');
 save('problems/SONA/results/S2K_Qd.mat', 'S2K_Qd');
 save('problems/SONA/results/K2S_Qd.mat', 'K2S_Qd');
 save('problems/SONA/results/S2K_astar.mat', 'S2K_astar');
@@ -156,7 +116,7 @@ err_kmeans = err_kmeans * 100;
 %Plot releveant data
 
 
-%Error
+            %Error
 
             figure;
             plot(K, err_transitive, 'r-o', 'LineWidth', 1)
@@ -182,6 +142,7 @@ err_kmeans = err_kmeans * 100;
             hold on;
             plot(K, t_astar, 'b-x', 'LineWidth',1)
             hold on;
+            plot(K, t_kmeans, 'g-*', 'LineWidth',1)
             hold off;
             xlabel('K')
             ylabel('time(sec.)');
@@ -193,13 +154,14 @@ err_kmeans = err_kmeans * 100;
             saveas(gcf, plot_name_png);
             
             
-            %Time to solveKDMP
+            %Time to solve KDMP
             
             figure;
             plot(K, t_KMDP_transitive, 'r-o', 'LineWidth', 1)
             hold on;
             plot(K, t_KMDP_astar, 'b-x', 'LineWidth',1)
             hold on;
+            plot(K, t_KMDP_kmeans, 'b-x', 'LineWidth',1)
             hold off;
             xlabel('K')
             ylabel('time(sec.)');
@@ -211,15 +173,29 @@ err_kmeans = err_kmeans * 100;
             saveas(gcf, plot_name_png);
 
 
-    %Explote data
+    %Represent data data
     
-    min_dens_aba = S(:,1);
-    max_dense_aba = S(:,2);
-    min_abun_so = S(:,3);
-    max_abun_so = S(:,4);
-    states = [1:819];
+S = readmatrix('problems/SONA/results/states.txt');
+S = S(:,1:4);
+
+
+%Explote data
     
-    
+min_dens_aba = S(:,1);
+max_dens_aba = S(:,2);
+min_abun_so = S(:,3);
+max_abun_so = S(:,4);
+states = [1:819];
+
+
+[min_dens_aba_u] = unique(min_dens_aba);
+[max_dens_aba_u] = unique(max_dens_aba);
+[min_abun_so_u] = unique(min_abun_so);
+[max_abun_so_u] = unique(max_abun_so);
+
+
+ 
+%Plot state space representation as a matrix.
 S_matrix = zeros(length(max_abun_so_u), length(min_dens_aba_u));
 it = 1;
 for x = 1:length(max_abun_so_u)
@@ -241,6 +217,9 @@ plot_name_png = strcat('problems/SONA/results/state-representation.png');
 saveas(gcf, plot_name_fig);
 saveas(gcf, plot_name_png);
 
+
+
+%Plot reduced state space representation as a matrix for K=5
 
 S_matrix_reduced = zeros(length(max_abun_so_u), length(min_dens_aba_u));
 it = 1;
@@ -267,6 +246,18 @@ plot_name_fig = strcat('problems/SONA/results/abstract-state-representation.fig'
 plot_name_png = strcat('problems/SONA/results/abstract-state-representation.png');
 saveas(gcf, plot_name_fig);
 saveas(gcf, plot_name_png);
+
+
+%Plot reduced model and policy graphs.
+
+
+%Name actions
+actions = strings(size(P,3), 1);
+actions(1) = "I";
+actions(2) = "AP";
+actions(3) = "C";
+actions(4) = "H";
+min_prob = 0.001;
 
 [G_reduced_model, p_reduced_model] = model_representation(PK_astar, RK_astar, actions, min_prob);
 [G_reduced_policy, p_reduced_policy] = policy_representation(PK_astar, RK_astar, PolicyK_astar, actions, 0.01);
